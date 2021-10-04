5dfe73fafdbc66ee7bc82fa9f1566dc80e837189
Lana Andrade <lanaandrade2003@gmail.com>
2021-10-03 22:07:22 -0300

    alteração em inserirMapaDeAmostras

--- NWAmostra_/src/main/java/crudMapaDeAmostras/inserirMapaDeAmostras.java
+++ NWAmostra_/src/main/java/crudMapaDeAmostras/inserirMapaDeAmostras.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDateTime;
 
 import conexao.Conexao;
 import jakarta.servlet.ServletException;
@@ -30,9 +29,12 @@ public class inserirMapaDeAmostras extends HttpServlet {
         	String caixa_mapa_amostra = request.getParameter("txtCaixaMapaDeAmostras");
         	Integer n_coluna_mapa_amostra = Integer.parseInt(request.getParameter("txtColunasMapaDeAmostras"));
         	Integer n_linha_mapa_amostra = Integer.parseInt(request.getParameter("txtLinhasMapaDeAmostras"));
+        	String data_inativacao_mapa_amostra = null;
 
-            String sqlInserirMapaDeAmostras = "INSERT INTO mapa_de_amostras (estante_mapa_amostra, nome_mapa_amostra, freezer_mapa_amostra, caixa_mapa_amostra, n_coluna_mapa_amostra, n_linha_mapa_amostra)"
-            + " VALUES ('" + estante_mapa_amostra  + "','" + nome_mapa_amostra + "','" + freezer_mapa_amostra + "','" + caixa_mapa_amostra + "','" + n_coluna_mapa_amostra + "','" + n_linha_mapa_amostra + "')";
+            String sqlInserirMapaDeAmostras = 
+            "INSERT INTO mapa_de_amostras (estante_mapa_amostra, nome_mapa_amostra, freezer_mapa_amostra, caixa_mapa_amostra, n_coluna_mapa_amostra, n_linha_mapa_amostra, data_inativacao_mapa_amostra)"
+            + " VALUES "
+            + "('" + estante_mapa_amostra  + "','" + nome_mapa_amostra + "','" + freezer_mapa_amostra + "','" + caixa_mapa_amostra + "','" + n_coluna_mapa_amostra + "','" + n_linha_mapa_amostra + "','" + data_inativacao_mapa_amostra + "')";
 
             Connection con = Conexao.conexao();
             Statement stInserirMapaDeAmostras = con.prepareStatement(sqlInserirMapaDeAmostras);
