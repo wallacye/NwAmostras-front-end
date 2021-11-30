b8d62a1d5dcd82227b740a56386c9fc088cb2134
Lana Andrade <lanaandrade2003@gmail.com>
2021-11-30 16:37:26 -0300

    criado inserir projeto

--- NWAmostra_/src/main/java/crudProjeto/inserirProjeto.java
+++ NWAmostra_/src/main/java/crudProjeto/inserirProjeto.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import conexao.Conexao;
+import model.MapaDeAmostras;
 import model.Pesquisador;
 import model.Projeto;
 
@@ -37,78 +38,75 @@ public class inserirProjeto extends HttpServlet
                 	Integer privado_publico_projeto = Integer.parseInt(request.getParameter("txtVisibilidadeProjeto"));		
                 	String pesquisadores = request.getParameter("txtAdicionarpesquisadorProjeto");			
                     java.sql.Date dt_inicio_projeto = new java.sql.Date(System.currentTimeMillis()); 
-                    Long id_para_converter;
                     int id_projeto;
-                    int id_pesq;
+                    long id_para_converter = -1L;
                     
                     List<String> listaPesquisadores = Arrays.asList(pesquisadores.split(","));
                                         
-                    Integer rodar = 1;
+                    Integer rodar = 0;
                     
                     Connection con = Conexao.Conectar();               
                     
-            	    String sqlInserirProjeto = "INSERT INTO amostra_no_mapa_contem (dt_termino_projeto, nome_projeto, dt_inicio_projeto, privado_publico_projeto)"
+            	    String sqlInserirProjeto = "INSERT INTO projeto (dt_termino_projeto, nome_projeto, dt_inicio_projeto, privado_publico_projeto)"
                             + "VALUES (?, ?, ?, ?)";
                     	    
                     	    PreparedStatement stInserirProjeto = con.prepareStatement(sqlInserirProjeto, Statement.RETURN_GENERATED_KEYS);
-                    	    
                     	    stInserirProjeto.setDate(1, null);
                     	    stInserirProjeto.setString(2, nome_projeto);
                     	    stInserirProjeto.setDate(3, dt_inicio_projeto);
                     	    stInserirProjeto.setInt(4, privado_publico_projeto);
+                    	    stInserirProjeto.executeUpdate();
 
                         try (ResultSet generatedKeys = stInserirProjeto.getGeneratedKeys()) 
                         {
-                                if (generatedKeys.next()) 
-                                {
-                                	id_para_converter = generatedKeys.getLong(1);
-                                	id_projeto = id_para_converter.intValue();  
-                                }
-                                else 
-                                {
-                                    throw new SQLException("CRIAR PROJETO::NENHUM ID FOI OBTIDO");
-                                }                    	    
-                    	    
-                    	    while(rodar <= listaPesquisadores.size()) 
-                    	    {
-                                String sqlExibirPesquisadores = "SELECT * FROM pesquisador WHERE email_pesq=" + listaPesquisadores.get(rodar);
+                            if (generatedKeys!=null && generatedKeys.next()) 
+                            {
+                                id_para_converter = generatedKeys.getLong(1);
+                                id_projeto = Math.toIntExact(id_para_converter);
                                 
-                                Statement stExibirPesquisadores = con.createStatement();
-                                ResultSet rsExibirPesquisadores = stExibirPesquisadores.executeQuery(sqlExibirPesquisadores);
-                                
-                                if ( rsExibirPesquisadores.next() )
-                                {
-                                	Pesquisador dados = new Pesquisador();
-                                	         	
-                                	id_pesq = rsExibirPesquisadores.getInt("id_pesq");
-                                	
-                                }
-                    	    	
-                    	    	String sqlInserirProjeto_Pesquisador = "INSERT INTO projeto_pesquisador_participa (fk_id_projeto, fk_id_pequisador)"
-                                + " VALUES (?, ?) ";
+                        	    while(rodar < listaPesquisadores.size()) 
+                        	    {
+                        	    	
+                                    String sqlExibirPesquisadores = "SELECT * FROM pesquisador WHERE email_pesq= '" +listaPesquisadores.get(rodar)+ "'";
+                                    
+                                    Statement stExibirPesquisadores = con.createStatement();
+                                    ResultSet rsExibirPesquisadores = stExibirPesquisadores.executeQuery(sqlExibirPesquisadores);
+                                    
+                                    if ( rsExibirPesquisadores.next() )
+                                    {                             	         	
+                                    	int id_pesq = rsExibirPesquisadores.getInt("id_pesq");
+                                    	
+                            	    	String sqlInserirProjeto_Pesquisador = "INSERT INTO projeto_pesquisador_participa (fk_id_projeto, fk_id_pequisador)"
+                                                + " VALUES (?, ?) ";
 
-                        	    PreparedStatement stInserirProjeto_Pesquisador = con.prepareStatement(sqlInserirProjeto_Pesquisador);
-                        	    stInserirProjeto_Pesquisador.setInt(1, id_projeto);
-                        	    stInserirProjeto_Pesquisador.setInt(2, id_pesq);
-                        	    
-                        	    stInserirProjeto_Pesquisador.executeUpdate();
-                        	    rodar++;
-                    	    } 
-                    	    }
+                                        	    PreparedStatement stInserirProjeto_Pesquisador = con.prepareStatement(sqlInserirProjeto_Pesquisador);
+                                        	    stInserirProjeto_Pesquisador.setInt(1, id_projeto);
+                                        	    stInserirProjeto_Pesquisador.setInt(2, id_pesq);                                        	    
+                                        	    stInserirProjeto_Pesquisador.executeUpdate();
+                                    }
+                            	    rodar++;                                    	
+                        	    	
+                        	    } 
+                            }
+                            else 
+                            {
+                                throw new SQLException("CRIAR PESQUISADOR_PROJETO::NENHUM ID FOI OBTIDO");
+                            }                   	    
+                    	    
+                    	}
                     	    catch (SQLException ex)
                     	    {
-                                out.print("Erro na conexão, inserir projeto: " + ex);
+                                out.print("Erro *parte de generated keys*: " + ex);
                     	    }
                 	                                       
-            	    
-                    	    response.sendRedirect("./jsp/jspLogado/telaInicialProjetos.jsp");
+                       response.sendRedirect("./jsp/jspLogado/telaInicialProjetos.jsp");
 
-                       } 
+                   } 
                        catch (SQLException ex) 
                        {
                             out.print("Erro na conexão, inserir projeto: " + ex);
                        } 
-                }
-
+                
     }
+
 }
